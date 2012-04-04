9b29e004db3138e29a0e5d9eafa152451778f335
Eric Meyer <emeyer@8thlight.com>
2012-04-03 19:43:54 -0500

    Passing Tests

--- MasterMind/MMCode.m
+++ MasterMind/MMCode.m
@@ -1,14 +1,49 @@
 #import "MMCode.h"
 
+@interface MMCode(private)
+-(NSArray*) stringToArray: (NSString*) string;
+@end
+
 @implementation MMCode
 
-
 -(NSNumber*) numberCorrectForCode: (NSString*) code andGuess: (NSString*) guess {
-    return [NSNumber numberWithInt: 123];
+    int numberCorrect = 0;
+    for (int i=0; i < [code length]; i++) {
+        if ([code characterAtIndex: i] == [guess characterAtIndex: i]) {
+            numberCorrect++;
+        }
+    }
+    return [NSNumber numberWithInt: numberCorrect];
 }
 
 -(NSNumber*) numberInWrongSpotForCode: (NSString*) code andGuess: (NSString*) guess {
-    return [NSNumber numberWithInt: 456];
+    int numberInWrongSpot = 0;
+    NSArray* codeAsArray = [self stringToArray: code];
+    NSArray* guessAsArray = [self stringToArray: guess];
+    NSMutableArray* incorrectCodeParts = [NSMutableArray array];
+    NSMutableArray* incorrectGuessParts = [NSMutableArray array];
+    for (int i=0; i < [guessAsArray count]; i++) {
+        if ([code characterAtIndex: i] != [guess characterAtIndex: i]) {
+            [incorrectCodeParts addObject: [codeAsArray objectAtIndex: i]];
+            [incorrectGuessParts addObject: [guessAsArray objectAtIndex: i]];
+        }
+    }
+    for (NSString* guessAtSpot in incorrectGuessParts) {
+        if ([incorrectCodeParts containsObject: guessAtSpot]) {
+            numberInWrongSpot++;
+            [incorrectCodeParts removeObject: guessAtSpot];
+        }
+    }
+    return [NSNumber numberWithInt: numberInWrongSpot];
+}
+
+-(NSArray*) stringToArray: (NSString*) string {
+    NSMutableArray *array = [[[NSMutableArray alloc] init] autorelease];
+    for (int i=0; i < [string length]; i++) {
+        NSString *spot  = [NSString stringWithFormat:@"%c", [string characterAtIndex:i]];
+        [array addObject: spot];
+    }
+    return array;
 }
 
 @end
--- Specs/MMCodeSpec.m
+++ Specs/MMCodeSpec.m
@@ -3,11 +3,46 @@
 
 CONTEXT(MMCode)
 {
-    describe(@"toBeFailing",
-             it(@"is a failling test",
-                ^{
-                    expectTruth(false);
+    __block MMCode* mmCode;
+    
+    describe(@"mastermind number correct",
+             beforeEach(^{
+                    mmCode = [[[MMCode alloc] init] autorelease];
                 }),
-             
-             nil); // Don't forget the terminating nil
+             it(@"has none right",
+                ^{
+                    NSNumber* numberCorrect = [mmCode numberCorrectForCode: @"1111" andGuess: @"2222"];
+                    [expect(numberCorrect) toBeEqualTo: [NSNumber numberWithInt: 0]];
+                }),
+             it(@"has 4 right",
+                ^{
+                    NSNumber* numberCorrect = [mmCode numberCorrectForCode: @"1111" andGuess: @"1111"];
+                    [expect(numberCorrect) toBeEqualTo: [NSNumber numberWithInt: 4]];
+                }),
+             it(@"has 2 right",
+                ^{
+                    NSNumber* numberCorrect = [mmCode numberCorrectForCode: @"2233" andGuess: @"2222"];
+                    [expect(numberCorrect) toBeEqualTo: [NSNumber numberWithInt: 2]];
+                }),
+             it(@"has 0 in the wrong spot when none are correct",
+                ^{
+                    NSNumber* numberInWrongSpot = [mmCode numberInWrongSpotForCode: @"1111" andGuess: @"2222"];
+                    [expect(numberInWrongSpot) toBeEqualTo: [NSNumber numberWithInt: 0]];
+                }),
+             it(@"has 1 in the wrong spot",
+                ^{
+                    NSNumber* numberInWrongSpot = [mmCode numberInWrongSpotForCode: @"1234" andGuess: @"2555"];
+                    [expect(numberInWrongSpot) toBeEqualTo: [NSNumber numberWithInt: 1]];
+                }),
+             it(@"doesn't count correct guesses as in the wrong spot",
+                ^{
+                    NSNumber* numberInWrongSpot = [mmCode numberInWrongSpotForCode: @"1234" andGuess: @"1555"];
+                    [expect(numberInWrongSpot) toBeEqualTo: [NSNumber numberWithInt: 0]];
+                }),
+             it(@"handles repeated characters when checking for the guesses in the wrong spot",
+                ^{
+                    NSNumber* numberInWrongSpot = [mmCode numberInWrongSpotForCode: @"1122" andGuess: @"1212"];
+                    [expect(numberInWrongSpot) toBeEqualTo: [NSNumber numberWithInt: 2]];
+                }),
+             nil);
 }
