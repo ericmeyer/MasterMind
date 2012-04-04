8878413e2d85c344c1d2fbd76701f8766deee08d
Eric Meyer <emeyer@8thlight.com>
2012-04-03 19:19:43 -0500

    GuessChecker AT Failing

---
+++ AcceptanceTests/GuessChecker.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+@interface GuessChecker : NSObject {
+    NSString* code;
+    NSString* guess;
+}
+@property (nonatomic, retain) NSString* code;
+@property (nonatomic, retain) NSString* guess;
+
+
+@end
---
+++ AcceptanceTests/GuessChecker.m
@@ -0,0 +1,15 @@
+#import "GuessChecker.h"
+
+@implementation GuessChecker
+
+@synthesize code, guess;
+
+-(int) numberCorrect {
+    return 123;
+}
+
+-(int) numberInWrongSpot {
+    return 456;
+}
+
+@end
